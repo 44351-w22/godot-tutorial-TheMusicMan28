ba4e9225cf4277766e5f2a30c9cabb4a8ed93713
Daniel J Crowley <dcrow2814@gmail.com>
2022-01-19 10:46:22 -0600

    added script for player

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+export var speed = 400 #number of pixels per second
+var screen_size #screen size. Duh
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+		
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+		
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
