5a3dea802a4c61258b7148042fb61717cfd68eec
Daniel J Crowley <dcrow2814@gmail.com>
2022-01-21 10:44:52 -0600

    Added in collision and position setting

--- Player.gd
+++ Player.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400 #number of pixels per second
 var screen_size #screen size. Duh
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -40,3 +43,14 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 		$AnimatedSprite.flip_h = false
+
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal('hit')
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
