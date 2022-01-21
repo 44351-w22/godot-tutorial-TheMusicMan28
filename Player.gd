721d3b5f694df8046b16b3f7ab3fc21ec7b13abf
Daniel J Crowley <dcrow2814@gmail.com>
2022-01-21 10:19:49 -0600

    added in animations

--- Player.gd
+++ Player.gd
@@ -23,7 +23,12 @@ func _process(delta):
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
 		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
+	
