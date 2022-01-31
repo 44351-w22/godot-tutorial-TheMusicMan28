79c539446ac9e0dae86bf13a610183adbc0ba1cc
Daniel J Crowley <dcrow2814@gmail.com>
2022-01-31 10:43:35 -0600

    connected hud to main

--- HUD.gd
+++ HUD.gd
@@ -18,3 +18,12 @@ func show_game_over():
 	
 func update_score(score):
 	$ScoreLabel.text = str(score)
+
+
+func _on_Message_Timer_timeout():
+	$Message.hide()
+
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	emit_signal("start_game")
